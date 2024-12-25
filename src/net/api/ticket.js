e332e4cff6ea9f29f8863c7e732f58de5fcaed7c
Nruonan <15231613@qq.com>
2024-12-25 19:47:55 +0800

    feat(net): 添加票务相关 API 接口

---
+++ src/net/api/ticket.js
@@ -0,0 +1,34 @@
+import {get, post} from "@/net/index.js";
+import {useStore} from "@/store/index.js";
+
+export const apiTicketTypes = () => {
+    const store = useStore()
+    get('/api/ticket/types',data=>{
+      store.ticket.types = data;
+    })
+}
+
+export const apiUserRemind = (success) =>
+    get('/api/user/privacy', success)
+export const apiUserRemindSave = (data,success) =>
+    post('/api/user/save-privacy', data, success)
+
+export const apiLoadTicketList = (page,type,name,success) =>
+    get(`/api/ticket/list?page=${page}&type=${type}&name=${name.value}`, success)
+export const apiTicketDetail = (id,success) =>
+    get(`/api/ticket?id=${id}`, success)
+
+export const apiTakeTicketOrder = (data,success) =>
+    post(`/api/ticket/save-order`, data, success)
+
+export const apiOrderSave = (data, success) =>
+    post(`/api/ticket/save-order`, data, success)
+
+export const apiOrderList = (id, success) =>
+    get(`/api/ticket/orders?uid=${id}`, success)
+
+export const apiOrderRepeat = (data, success) =>
+    post(`/api/ticket/order-repeat`, data, success)
+
+export const apiOrderRemove = (data, success) =>
+    post(`/api/ticket/remove-order`, data, success)
